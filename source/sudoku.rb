cf2937c36c9a6dea55607602f13f7633a863a61e
Brick Thornton <brickthornton@gmail.com>
2014-09-25 11:33:11 -0700

    integrate new boards and torey edits

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -2,24 +2,15 @@ class Sudoku
   def initialize(board_string)
   end
 
-  def solve!
+  def solve
   end
 
-  # Returns a string representing the current state of the board
-  # Don't spend too much time on this method; flag someone from staff
-  # if you are.
   def board
+  end
 
+  # Returns a nicely formatted string representing the current state of the board
+  def to_s
   end
 end
 
-# The file has newlines at the end of each line, so we call
-# String#chomp to remove them.
-board_string = File.readlines('sample.unsolved.txt').first.chomp
 
-game = Sudoku.new(board_string)
-
-# Remember: this will just fill out what it can and not "guess"
-game.solve!
-
-puts game.board
